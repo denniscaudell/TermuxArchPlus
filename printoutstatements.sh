365e61c6c65b2ac70fbaf64508b7ff21692c6704
S D Rausty <sdrausty@gmail.com>
2017-11-03 14:01:38 -0400

    Commit on 20171103 at 14:01:38 and at 1509732098 in Unix time.

--- printoutstatements.sh
+++ printoutstatements.sh
@@ -8,7 +8,7 @@
 
 printdetectedsystem ()
 {
-	printf "\033[36;1m\n  Detected $(uname -mo) Operating System ✈  \n"
+	printf "\033[36;1m\n  Detected $(uname -mo) Operating System \033[37;1m✈\033[33;1m  \n"
 }
 
 printdownloading ()
