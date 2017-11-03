61802e81099d04d9d9ef4b6b7561ec66193f5526
S D Rausty <sdrausty@gmail.com>
2017-11-02 22:54:13 -0400

    Commit on 20171102 at 22:54:13 and at 1509677653 in Unix time.

--- necessaryfunctions.sh
+++ necessaryfunctions.sh
@@ -92,7 +92,7 @@ detectsystem2 ()
 makebin ()
 {
 	cat > $bin <<- EOM
-	#!/data/data/com.termux/files/usr/bin/sh -e
+	#!/bin/sh -e
 	unset LD_PRELOAD
 	exec proot --link2symlink -0 -r $HOME/arch/$sysp -b /dev/ -b /sys/ -b /proc/ -b /storage/ -b $HOME -w $HOME /bin/env -i HOME=/root TERM="$TERM" PS1='[termux@arch \W]\$ ' LANG=$LANG PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash --login
 	EOM
