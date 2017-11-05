7db26898a21012ec98f21d43d1e336cce9efc7ae
S D Rausty <sdrausty@gmail.com>
2017-11-05 12:50:37 -0500

    Commit on 20171105 at 12:50:37 and at 1509904237 in Unix time.

--- docs/setupTermuxArch.sh
+++ docs/setupTermuxArch.sh
@@ -6,7 +6,7 @@
 # Change mirror to your desired geographic location in `knownconfigurations.sh`.
 ################################################################################
 
-printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n\033[76;1m\033[46;1m 🕛 This setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be at the bash prompt in Arch Linux in Termux.  Updating Termux and installing the required components for Arch Linux installation.  🔆  \n\n"
+printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n\033[76;1m\033[46;1m 🕛 This setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be at the bash prompt in Arch Linux in Termux.  Updating Termux and installing the required components for Arch Linux installation.  \n\n"
 
 apt-get -qq update && apt-get -qq upgrade --yes
 apt-get -qq install bsdtar proot termux-exec wget --yes 
--- printoutstatements.sh
+++ printoutstatements.sh
@@ -13,7 +13,7 @@ printdetectedsystem ()
 
 printdownloading ()
 {
-	printf "\033[34;1m\033[44;1m\n 🕜 Now downloading \033[36;1m$file\033[34;1m and the corresponding checksum 🐬  This may take a long time depending on your Internet speed 🚤  Be patient ⛵  If you are going to set your device aside, ensure \033[36;1mtermux-wake-lock\033[34;1m is active to let this process complete.  🏄  \n\n"
+	printf "\033[34;1m\033[44;1m\n 🕜 Now downloading \033[36;1m$file\033[34;1m and the corresponding checksum.  This may take a long time depending on your Internet speed.  Be patient.  If you are going to set your device aside, ensure \033[36;1mtermux-wake-lock\033[34;1m is active to let this process complete.  \n\n"
 }
 
 printfooter()
@@ -23,7 +23,7 @@ printfooter()
 
 printmd5check ()
 {
-	printf "\033[33;1m\033[43;1m\n 🕠 Checking download integrity with \033[34;1mmd5sum\033[33;1m 🐚  This may take a little while.  \n\n 🕕 "
+	printf "\033[33;1m\033[43;1m\n 🕠 Checking download integrity with \033[34;1mmd5sum\033[33;1m.  This may take a little while.  \n\n 🕕 "
 }
 
 printmd5error ()
@@ -33,7 +33,7 @@ printmd5error ()
 
 printmd5success ()
 {
-	printf "\033[32;1m\033[42;1m\n 🕖 Now uncompressing \033[33;1m$file\033[32;1m.  \033[33;1mThis will take much longer!  Be patient ⛺🌴  \033[32;1m  If you are going to set your device aside, ensure \033[33;1mtermux-wake-lock\033[32;1m is active to let this process complete.  🌲🏡  \n"
+	printf "\033[32;1m\033[42;1m\n 🕖 Now uncompressing \033[33;1m$file\033[32;1m.  \033[33;1mThis will take much longer!  Be patient.  \033[32;1m  If you are going to set your device aside, ensure \033[33;1mtermux-wake-lock\033[32;1m is active to let this process complete.  \n"
 }
 
 printmd5syschkerror ()
