40db95a5d4a2d98c853de10c29e2b013d50249ea
S D Rausty <sdrausty@gmail.com>
2017-11-04 21:21:08 -0400

    Commit on 20171104 at 21:21:08 and at 1509844868 in Unix time.

--- printoutstatements.sh
+++ printoutstatements.sh
@@ -13,7 +13,7 @@ printdetectedsystem ()
 
 printdownloading ()
 {
-	printf "\033[34;1m\n 🕑 Now downloading \033[36;1m\`$file\`\033[34;1m and the corresponding checksum 🚢  This may take a long time depending on your Internet speed 🚤  Be patient ⛵  If you are going to set your device aside, ensure \033[36;1m\`termux-wake-lock\`\033[34;1m is active to let this process complete.	🐬  \n\n"
+	printf "\033[34;1m\n 🕑 Now downloading \033[36;1m\`$file\`\033[34;1m and the corresponding checksum 🏄  This may take a long time depending on your Internet speed 🚤  Be patient ⛵  If you are going to set your device aside, ensure \033[36;1m\`termux-wake-lock\`\033[34;1m is active to let this process complete.	🐬  \n\n"
 }
 
 printfooter()
