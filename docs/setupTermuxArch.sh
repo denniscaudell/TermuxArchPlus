1c9f9ef7d4bbaedc065e488a045326ffbca11ffe
S D Rausty <sdrausty@gmail.com>
2017-11-06 11:45:08 -0500

    Commit on 20171106 at 11:45:08 and at 1509986708 in Unix time.

---
+++ docs/setupTermuxArch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+# See https://sdrausty.github.io/TermuxArch/Contributors Thank You! 
+# Website for this project at https://sdrausty.github.io/TermuxArch
+# Copyright 2017 by SDRausty. All rights reserved.
+# 🌍 🌎 🌏 🌐 🗺
+# Change mirror to your desired geographic location in `knownconfigurations.sh`.
+################################################################################
+
+printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n\033[32;1m 🕛 This setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be at the bash prompt in Arch Linux in Termux.  Updating Termux and installing the required components for Arch Linux installation.  \n\n"
+
+apt-get -qq update && apt-get -qq upgrade --yes
+apt-get -qq install bsdtar proot termux-exec wget --yes 
+
+wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/knownconfigurations.sh
+wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/necessaryfunctions.sh
+wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/printoutstatements.sh
+wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/setupTermuxArch.sh
+wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/termuxarchchecksum.md5
+. ./knownconfigurations.sh
+. ./necessaryfunctions.sh
+. ./printoutstatements.sh
+
+# Main Block
+detectsystem 
+$HOME/arch/$bin ||:
+printtail
+exit 
+
